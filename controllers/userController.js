dadd4538c209a5b2401ad5b12f0e51f9e40b1a97
Eryk Ciesielski <eryk0399@gmail.com>
2022-10-18 11:09:51 +0200

    fix error if statements in userController login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,78 +1,65 @@
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const { json } = require('express');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { json } = require("express");
 
 const register = async (req, res) => {
-    try{
-        if(exists(req.body.username))
-        {
-            res.status(400),json({message: 'invalid user data'});
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  try {
+    if (exists(req.body.username)) {
+      res.status(400), json({ message: "invalid user data" });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        const user = await User.create({
-            username: req.body.username,
-            password: hashedPassword
-        });
-        res.status(201).json({
-            username: user.username
-        })
-        
+      const user = await User.create({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      res.status(201).json({
+        username: user.username,
+      });
     }
-    catch(err){
-        res.status(500).json({message: err.message})
-        console.log(err.message)
-    }
-    
-
-}
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log(err.message);
+  }
+};
 const login = async (req, res) => {
-    const {username, password} = req.body;
-    const user = await User.findOne({username: username})
+  const { username, password } = req.body;
+  const user = await User.findOne({ username: username });
 
-    if(user && (await bcrypt.compare(password, user.password)))
-    {
-        res.status(200).json({
-            username: user.username,
-            token: generateToken(user._id),
-        })
-    }
-    else{
-        res.status(400).json({message: 'invlaid password or user name'})
-    }
-    
-}
-const me = async(req, res) => {
-    try{
-        res.status(200).json({
-            username: req.user.username
-        })
-    }
-    catch(err)
-    {
-        res.status(401).json({message: 'unathorized'})
-        console.log(err.message)
-    }
-    
-    
-}
-
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      username: user.username,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message: "invlaid password or user name" });
+  }
+};
+const me = async (req, res) => {
+  try {
+    res.status(200).json({
+      username: req.user.username,
+    });
+  } catch (err) {
+    res.status(401).json({ message: "unathorized" });
+    console.log(err.message);
+  }
+};
 
 //Private functions
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.SECRET, {
-        expiresIn: '60m'
-    })
-}
+  return jwt.sign({ id }, process.env.SECRET, {
+    expiresIn: "60m",
+  });
+};
 const exists = async (username) => {
-    const user = await User.find({username: username});
-    if(user.length>0)
-    {
-        return true;
-    }
-    return false;
-}
+  const user = await User.find({ username: username });
+  if (user.length > 0) {
+    return true;
+  }
+  return false;
+};
 
-module.exports = {register, login, me};
+module.exports = { register, login, me };
